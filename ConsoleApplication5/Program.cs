751837fb6b7ce599d592bbbdfd7f234792971319
atma0004 <atma0004@edu.eal.dk>
2017-05-30 21:55:16 +0200

    Add project files.

---
+++ ConsoleApplication5/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            print pt = new print(printt);
+            
+            pt(20);
+            pt = pt + new print(printt);
+        }
+        delegate void print(int str);
+        static void pr(string s)
+        {
+            Console.WriteLine(s);
+        }
+        static void printt(int k)
+        {
+            if(k<40)
+            File.WriteAllText("C:\\Users\\dj\\Desktop\\latestversion\\txt\\kkkpppkk.txt", k.ToString());
+        }
+    }
+}
